83315961337588f47b405e565a287dda3c6b9d09
s-beats <sakashin10291029@gmail.com>
2022-06-04 21:12:23 +0900

    feat: command

---
+++ command.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli/v2"
+)
+
+func cmdGenerateUUID() *cli.Command {
+	return &cli.Command{
+		Name:  "uuid",
+		Usage: "generate uuid",
+		Action: func(c *cli.Context) error {
+			id := generateUUID()
+			fmt.Printf("uuid: %s\n", id)
+			return nil
+		},
+	}
+}
+
+func cmdGenerateULID() *cli.Command {
+	return &cli.Command{
+		Name:  "ulid",
+		Usage: "generate ulid",
+		Action: func(c *cli.Context) error {
+			id := generateULID()
+			fmt.Printf("ulid: %s\n", id)
+			return nil
+		},
+	}
+}
