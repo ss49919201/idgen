0e36f3113a630cc618930e4d070dd91daaef5802
s-beats <sakashin10291029@gmail.com>
2022-09-07 21:28:38 +0900

    feat: usage command

--- command.go
+++ command.go
@@ -9,7 +9,7 @@ import (
 func cmdGenerateUUID() *cli.Command {
 	return &cli.Command{
 		Name:  "uuid",
-		Usage: "generate uuid",
+		Usage: "Generate UUID.",
 		Action: func(c *cli.Context) error {
 			id := generateUUID()
 			fmt.Printf("UUID: %s\n", id)
@@ -21,7 +21,7 @@ func cmdGenerateUUID() *cli.Command {
 func cmdGenerateULID() *cli.Command {
 	return &cli.Command{
 		Name:  "ulid",
-		Usage: "generate ulid",
+		Usage: "Generate ULID.",
 		Action: func(c *cli.Context) error {
 			id := generateULID()
 			fmt.Printf("ULID: %s\n", id)
