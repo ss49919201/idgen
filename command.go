6d377121afbb07a504d1d3ccdd70b15622ed05bb
Shinya Sakae <ss49919201@gmail.com>
2025-02-21 08:41:32 +0900

    feat: add support for generating multiple UUIDs and ULIDs

--- command.go
+++ command.go
@@ -1,21 +1,75 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 
+	"github.com/google/uuid"
+	"github.com/oklog/ulid/v2"
 	"github.com/urfave/cli/v2"
 )
 
 var writer = os.Stdout
 
+var numberOfIDsFlag = &cli.IntFlag{
+	Name:        "number",
+	Aliases:     []string{"n"},
+	Usage:       "Number of IDs to generate",
+	Value:       1,
+	DefaultText: "1",
+}
+
+func parseNumberOfIDsFlag(c *cli.Context) (*int, error) {
+	n := c.String("number")
+
+	parsedN, err := strconv.Atoi(n)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse number flag: %w", err)
+	}
+
+	if parsedN <= 0 {
+		return nil, errors.New("number flag must be greater than 0")
+	}
+
+	return &parsedN, nil
+}
+
+func generateMultiIDs[T any](generateId func() (T, error), number int) ([]T, error) {
+	ids := make([]T, 0, number)
+	for i := 0; i < number; i++ {
+		id, err := generateId()
+		if err != nil {
+			return nil, err
+		}
+		ids = append(ids, id)
+	}
+	return ids, nil
+}
+
 func cmdGenerateUUID() *cli.Command {
 	return &cli.Command{
 		Name:  "uuid",
 		Usage: "Generate UUID.",
+		Flags: []cli.Flag{
+			numberOfIDsFlag,
+		},
 		Action: func(c *cli.Context) error {
-			id := generateUUID()
-			fmt.Fprintln(writer, id)
+			number, err := parseNumberOfIDsFlag(c)
+			if err != nil {
+				return err
+			}
+
+			ids := make([]uuid.UUID, 0, *number)
+			for i := 0; i < *number; i++ {
+				ids = append(ids, generateUUID())
+			}
+
+			for i := 0; i < len(ids); i++ {
+				fmt.Fprintln(writer, ids[i])
+			}
+
 			return nil
 		},
 	}
@@ -25,12 +79,28 @@ func cmdGenerateUUIDV7() *cli.Command {
 	return &cli.Command{
 		Name:  "uuidv7",
 		Usage: "Generate UUID V7.",
+		Flags: []cli.Flag{
+			numberOfIDsFlag,
+		},
 		Action: func(c *cli.Context) error {
-			id, err := generateUUIDV7()
+			number, err := parseNumberOfIDsFlag(c)
 			if err != nil {
 				return err
 			}
-			fmt.Fprintln(writer, id)
+
+			ids := make([]uuid.UUID, 0, *number)
+			for i := 0; i < *number; i++ {
+				id, err := generateUUIDV7()
+				if err != nil {
+					return err
+				}
+				ids = append(ids, id)
+			}
+
+			for i := 0; i < len(ids); i++ {
+				fmt.Fprintln(writer, ids[i])
+			}
+
 			return nil
 		},
 	}
@@ -40,9 +110,24 @@ func cmdGenerateULID() *cli.Command {
 	return &cli.Command{
 		Name:  "ulid",
 		Usage: "Generate ULID.",
+		Flags: []cli.Flag{
+			numberOfIDsFlag,
+		},
 		Action: func(c *cli.Context) error {
-			id := generateULID()
-			fmt.Fprintln(writer, id)
+			number, err := parseNumberOfIDsFlag(c)
+			if err != nil {
+				return err
+			}
+
+			ids := make([]ulid.ULID, 0, *number)
+			for i := 0; i < *number; i++ {
+				ids = append(ids, generateULID())
+			}
+
+			for i := 0; i < len(ids); i++ {
+				fmt.Fprintln(writer, ids[i])
+			}
+
 			return nil
 		},
 	}
